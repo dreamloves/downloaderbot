10923ac04bf6a35770a0bb44bba4d30642c6be34
Pkhaxzor <146362739+Pkhaxzor@users.noreply.github.com>
2023-10-30 07:04:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -486,7 +486,7 @@ async def account_login(bot: Client, m: Message):
                 cmd = f'yt-dlp -f "{ytf}+bestaudio" --hls-prefer-ffmpeg --no-keep-video --no-check-certificate --remux-video mkv "{url}" -o "{name}.%(ext)s"'
             print(cmd)
             try:
-                Show = f"**Bot made by Pankaj Yadav**\n\n**Name :-** `{name}\nQuality - {raw_text2}`\n\n**Url :-** `{url}`"
+                Show = f"**Devloper Pankaj Yadav 💻**\n\n**Name :-** `{name}\nQuality - {raw_text2}`\n\n**Url :-** `{url}`"
                 prog = await m.reply_text(Show)
                 cc = f'{str(count).zfill(3)}**.** {name1} {res}\n\n**Batch :-** {raw_text0}'
                 cc1 = f'{str(count).zfill(3)}**.** {name1} {res}.pdf\n\n**Batch :-** {raw_text0}'
@@ -515,7 +515,7 @@ async def account_login(bot: Client, m: Message):
                         ka = await helper.aio(url, name)
                         await prog.delete(True)
                         time.sleep(1)
-                        reply = await m.reply_text(f"Uploading 📥- ```{name}```")
+                        reply = await m.reply_text(f"Haxzor")
                         time.sleep(1)
                         start_time = time.time()
                         await m.reply_document(
